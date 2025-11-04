f28682e6e8b5540e8cc3ae09ac3a81f4f0549101
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-11-05 00:10:44 +0700

    update

--- app/components/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="bg-gray-800 text-white p-4 mt-8">
-    <div class="container mx-auto text-center">
-      <p>&copy; {{ new Date().getFullYear() }} IATTA Adventure Travel. All rights reserved.</p>
-    </div>
-  </footer>
-</template>
--- app/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <AppNavbar />
-    <main class="container mx-auto p-4 min-h-screen">
-      <slot />
-    </main>
-    <AppFooter />
-  </div>
-</template>
